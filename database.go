5df76213067597d7be7ba3ea087b6d95db411044
corrupt <corrupt@giggedy.de>
2015-02-06 17:15:12 +0100

    initial commit

---
+++ database.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+var (
+	table_tweets = "CREATE TABLE IF NOT EXISTS tweets (ID INTEGER PRIMARY KEY AUTOINCREMENT, URL TEXT UNIQUE NOT NULL, HEADLINE TEXT DEFAULT NULL);"
+)
+
+type Tweet struct {
+	url      string `db:"url"`
+	headline string `db:"headline"`
+}
+
+func dbInit() (err error) {
+	db := dbConnect()
+	defer dbDisconnect(db)
+
+	log.Println("Trying to ping database")
+	err = db.Ping()
+	if err != nil {
+		return err
+	} else {
+		log.Println("Database ping successful")
+	}
+
+	err = schemaInit(db)
+	if err != nil {
+		return err
+	} else {
+		log.Println("Schema creation/validation successful")
+	}
+
+	return nil
+}
+
+func dbConnect() (db *sql.DB) {
+	db, err := sql.Open("sqlite3",
+		database_location)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func dbDisconnect(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func schemaInit(db *sql.DB) error {
+	_, err := db.Exec(table_tweets)
+	return err
+}
+
+func prepareInsertStatement(db *sql.DB) (insertStmt *sql.Stmt) {
+	insertStmt, err := db.Prepare("INSERT INTO tweets (url, headline) VALUES (?,?);")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return insertStmt
+}
+
+func prepareSelectStatement(db *sql.DB) (selectStmt *sql.Stmt) {
+	selectStmt, err := db.Prepare("SELECT url,headline FROM tweets WHERE url = ?;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return selectStmt
+}
+
+func insertTweet(tweet Tweet) error {
+	db := dbConnect()
+	defer dbDisconnect(db)
+	stmt := prepareInsertStatement(db)
+	defer stmt.Close()
+
+	log.Println("Inserting '" + tweet.headline + "'")
+	_, err := stmt.Exec(tweet.url, tweet.headline)
+	return err
+}
+
+func getTweetByUrl(url string) (tweet *Tweet, err error) {
+	tweet = &Tweet{}
+	db := dbConnect()
+	defer dbDisconnect(db)
+	stmt := prepareSelectStatement(db)
+	defer stmt.Close()
+
+	log.Println("Trying to retrieve '" + url + "'")
+	err = stmt.QueryRow(url).Scan(&(tweet.url), &(tweet.headline))
+	return tweet, err
+}
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/ChimeraCoder/anaconda"
+	//"errors"
+	"github.com/corrupt/anaconda"
+	rss "github.com/jteeuwen/go-pkg-rss"
+	"golang.org/x/net/html/charset"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+var api = anaconda.NewTwitterApi(AccessToken, AccessTokenSecret)
+
+func main() {
+	anaconda.SetConsumerKey(ConsumerKey)
+	anaconda.SetConsumerSecret(ConsumerSecret)
+
+	go updateConfiguration()
+	err := dbInit()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	PollFeed("http://www.metalstorm.net/rss/news.xml", 5)
+}
+
+func PollFeed(uri string, timeout int) {
+	feed := rss.New(timeout, true, chanHandler, itemHandler)
+
+	for {
+		if err := feed.Fetch(uri, getCharsetReader); err != nil {
+			fmt.Fprintf(os.Stderr, "[e] %s: %s", uri, err)
+			return
+		}
+
+		<-time.After(time.Duration(feed.SecondsTillUpdate() * 1e9))
+	}
+}
+
+func getCharsetReader(contentType string, input io.Reader) (io.Reader, error) {
+	reader, err := charset.NewReader(input, contentType)
+	return reader, err
+}
+
+func chanHandler(feed *rss.Feed, newchannels []*rss.Channel) {
+	//fmt.Printf("%d new channel(s) in %s\n", len(newchannels), feed.Url)
+}
+
+func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
+	for _, item := range newitems {
+		tweet := Tweet{item.Links[0].Href, shortenTweet(item.Title)}
+		tweetHandler(tweet)
+	}
+}
+
+func tweetHandler(tweet Tweet) (err error) {
+
+	twt, err := getTweetByUrl(tweet.url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if twt != nil && twt.url == tweet.url {
+		//return errors.New("Tweet '" + tweet.headline + "' is already cached")
+		log.Println("Tweet '" + tweet.headline + "' is already cached")
+	} else {
+		_, err = api.PostTweet(tweet.headline+"\n"+tweet.url, nil)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			err = insertTweet(tweet)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
+}
+
+func shortenTweet(tweet string) string {
+	txtlen := 140 - linklength - 1 //tweet length - t.co link length - \n
+	if len(tweet) > txtlen {
+		return tweet[:txtlen-3] + "..."
+	}
+	return tweet
+}
+
+func updateConfiguration() {
+	for {
+		log.Println("Updating Twitter configuration cache")
+		config, err := api.GetConfiguration(nil)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			linklength = config.ShortUrlLengthHttps
+		}
+		<-time.After(24 * time.Hour)
+	}
+}
